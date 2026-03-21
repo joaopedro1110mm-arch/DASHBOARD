477f42b7b145f4a92e8bb13ef445bdcfdf39139d
joaopedro1110mm-arch <joaopedro1110mm@gmail.com>
2026-03-21 13:37:45 +0000

    alteraçao

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-import plotly
 alunos = pd.read_excel(r"C:\Users\Aluno\Pictures\__pycache__\Vendas.xlsx")
 st.set_page_config("dashboards","💎",layout=("wide"))
 st.title("ola bem vindos a pagina de relatorios")
